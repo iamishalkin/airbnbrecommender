50c9d9e8852c3ea8023d54e48a06753108346c34
Ivan <myjiayug3@gmail.com>
2017-02-15 16:06:46 +0300

    added review data

--- recommender.r
+++ recommender.r
@@ -1,1 +1,7 @@
+#https://muffynomster.wordpress.com/2015/06/07/building-a-movie-recommendation-engine-with-r/
 library(recommenderlab)
+
+#1 try with spb data
+#create item matrix at first without preprocessing
+library(readr)
+spbaprt <- read_csv("Saint-Petersburg--Russia.csv")
