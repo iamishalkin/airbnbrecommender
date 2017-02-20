597a45aef1efe389a06141b4fe597d9da5a1cee9
Ivan <myjiayug3@gmail.com>
2017-02-20 10:33:06 +0300

    Add klg and some notes on user profile

--- recommender.r
+++ recommender.r
@@ -5,9 +5,10 @@ library(recommenderlab)
 #1 try with spb data
 #create item matrix at first without preprocessing
 library(readr)
-
+library(dplyr)
 spbapart <- read_csv("Data-city/Saint-Petersburg--Russia.csv")
 mskapart=read_csv("Data-city/Moscow--Russia.csv")
+klgapart=read_csv("Data-city/Kaliningrad--Kaliningrad-Oblast--Russia.csv")
 #df<- read_csv("Saint-Petersburg--Russia.csv")
 selectcolumns=function(df){
   library(dplyr)
@@ -23,13 +24,15 @@ selectcolumns=function(df){
 }
 spbapart=selectcolumns(spbapart)
 mskapart=selectcolumns(mskapart)
+klgapart=selectcolumns(klgapart)
 one_city_apart_fix_price=function(apart){
  apart$fixed_price=apart$price_native- mean(apart$price_native)
  return(apart)
 }
 mskapart=one_city_apart_fix_price(mskapart)
 spbapart=one_city_apart_fix_price(spbapart)
-aparts=rbind(mskapart, spbapart)
+klgapart=one_city_apart_fix_price(klgapart)
+aparts=rbind(mskapart, spbapart, klgapart)
 #now users
 #load reviews
 
@@ -38,10 +41,26 @@ spbreview <- read_csv("Data-review/Review_Saint-Petersburg--Russia.csv")
 
 mskreview=read_csv("Data-review/Review_Moscow--Russia.csv")
 review=rbind(spbreview,mskreview)
-
+review <- review[!duplicated(review$review),]#delete dublicates
 user_reviews=review[,1:2]
 
+#find the most active user of airbnb
+users_activity=user_reviews %>% group_by(author_id) %>% summarise(count=n())
+#the moast active user is 16262021 (11 reviews) but only one commmodity
+#the next 4155392 and he evaluated different aparts. Nice!
+
+#get all users' apartment features
 user_reviews=inner_join(user_reviews, aparts, by='apart_id')
+
+#create user profile
+profile=filter(user_reviews, author_id==4155392)
+#to create a profile we should count mean for all integers and moda for all factor variables
+
+
+
+#we would like to recommend him apart in kaliningrad
+
+
 
 library(reshape2)
 user_apart_df=dcast(review, apart_id ~ author_id, length)
